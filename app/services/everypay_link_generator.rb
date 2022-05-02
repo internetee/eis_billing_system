4edabbac29555c05c1ba239b265dc2698474c940
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2022-05-02 16:28:06 +0300

    implement link builder in app

--- app/services/everypay_link_generator.rb
+++ app/services/everypay_link_generator.rb
@@ -2,25 +2,26 @@ module EverypayLinkGenerator
   extend self
 
   def create(params:)
-    everypay_params = everypay_params(params)
+    everypay_params = linkpay_params(params)
 
-    linker = EverypayV4Wrapper::LinkBuilder.new(key: GlobalVariable::KEY, params: everypay_params)
-
-    linker.build_link
+    build_link(everypay_params.to_query)
   end
 
-  def everypay_params(params)
-    {
-      transaction_amount: params[:transaction_amount].to_s,
-      order_reference: params[:invoice_number].to_s,
-      customer_name: SimpleIDN.to_unicode(params[:customer_name].to_s), # buyer_name
-      customer_email: params[:customer_email].to_s, # buyer_email
-      custom_field_1: params.fetch(:custom_field1, ''),
-      custom_field_2: params[:custom_field2].to_s,
-      invoice_number: params[:invoice_number].to_s,
-      linkpay_token: GlobalVariable::LINKPAY_TOKEN
-    }
+  def linkpay_params(params)
+    { 'transaction_amount' => params[:transaction_amount].to_s,
+      'order_reference' => params[:invoice_number].to_s,
+      'customer_name' => params[:customer_name].to_s.parameterize(separator: '%20', preserve_case: true),
+      'customer_email' => params[:customer_email].to_s,
+      'custom_field_1' => params.fetch(:custom_field1, '').to_s.parameterize(separator: '%20', preserve_case: true),
+      'custom_field_2' => params[:custom_field2].to_s.parameterize(separator: '%20', preserve_case: true),
+      'linkpay_token' => GlobalVariable::LINKPAY_TOKEN,
+      'invoice_number' => params[:invoice_number].to_s }
+  end
+
+  def build_link(params)
+    data = CGI.unescape(params)
+    hmac = OpenSSL::HMAC.hexdigest('sha256', GlobalVariable::KEY, data)
+
+    "https://igw-demo.every-pay.com/lp?#{CGI.unescape(data)}&hmac=#{hmac}"
   end
 end
-
-# https://igw-demo.every-pay.com/lp?custom_field_1=prepended&custom_field_2=auction&customer_email=timo.vohmar@eestiinternet.ee&customer_name=mary_%C3%A4nn_o%E2%80%99conne%C5%BE-%C5%A1uslik_testnumber&invoice_number=160022&linkpay_token=k5t5xq&order_reference=160022&transaction_amount=6.00&hmac=2910ac02460da34eeedb1b5e248360a6d4e2d636bf727f2291e235d62ac3505f
