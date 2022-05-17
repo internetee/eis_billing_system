8e97bae22debbb5438adbab7f12cae6858eeaeb8
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2022-05-17 13:39:03 +0300

    fixed encoding utf-8 letters

--- app/services/everypay_link_generator.rb
+++ app/services/everypay_link_generator.rb
@@ -9,24 +9,27 @@ class EverypayLinkGenerator
     fetcher = new(params: params)
     everypay_params = fetcher.linkpay_params(params)
 
-    fetcher.build_link(everypay_params.to_query)
+    res = HttpBuildQuery.create everypay_params
+    res.gsub!('+', '%20')
+
+    # fetcher.build_link(everypay_params.to_query)
+    fetcher.build_link(res)
   end
 
   def linkpay_params(params)
     { 'transaction_amount' => params[:transaction_amount].to_s,
       'order_reference' => params[:invoice_number].to_s,
-      'customer_name' => params[:customer_name].to_s.parameterize(separator: '%20', preserve_case: true),
+      'customer_name' => params[:customer_name].to_s,
       'customer_email' => params[:customer_email].to_s,
-      'custom_field_1' => params.fetch(:custom_field1, '').to_s.parameterize(separator: '%20', preserve_case: true),
-      'custom_field_2' => params[:custom_field2].to_s.parameterize(separator: '%20', preserve_case: true),
+      'custom_field_1' => params.fetch(:custom_field1, '').to_s,
+      'custom_field_2' => params[:custom_field2].to_s,
       'linkpay_token' => GlobalVariable::LINKPAY_TOKEN,
       'invoice_number' => params[:invoice_number].to_s }
   end
 
   def build_link(params)
-    data = CGI.unescape(params)
-    hmac = OpenSSL::HMAC.hexdigest('sha256', GlobalVariable::KEY, data)
+    hmac = OpenSSL::HMAC.hexdigest('sha256', GlobalVariable::KEY, params)
 
-    "https://igw-demo.every-pay.com/lp?#{CGI.unescape(data)}&hmac=#{hmac}"
+    "https://igw-demo.every-pay.com/lp?#{params}&hmac=#{hmac}"
   end
 end
