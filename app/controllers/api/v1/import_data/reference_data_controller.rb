1d63d688de88a6a8d39f802726dd0a893b836127
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2022-06-15 13:33:27 +0300

    refactored import data job

--- app/controllers/api/v1/import_data/reference_data_controller.rb
+++ app/controllers/api/v1/import_data/reference_data_controller.rb
@@ -1,8 +1,16 @@
-class Api::V1::ImportData::ReferenceDataController < ApplicationController
-  def create
-    data = SaveReferenceDataJob.perform_now(params["_json"])
+module Api
+  module V1
+    module ImportData
+      class ReferenceDataController < ApplicationController
+        def create
+          data = SaveReferenceDataJob.perform_now(params['_json'])
+          message = "Added #{data[0]} reference numbers, skipped #{data[1]}." \
+                 ' The reason for omission may be that the reference data is' \
+                 'already in the database or the information is incorrect'
 
-    render status: 200,
-           json: { message: "Added #{data[0]} reference numbers, skipped #{data[1]}. The reason for omission may be that the reference data is already in the database or the information is incorrect" }
+          render status: :ok, json: { message: message }
+        end
+      end
+    end
   end
 end
---
+++ app/jobs/save_invoice_data_job.rb
@@ -0,0 +1,74 @@
+class SaveInvoiceDataJob < ApplicationJob
+  def perform(response)
+    parse_response(response)
+  end
+
+  private
+
+  def parse_response(response)
+    added_count = 0
+    skipped_count = 0
+    data_collection = {}
+
+    response.each do |data|
+      data_collection = collect_response_data(data)
+
+      if data_collection[:invoice_number].nil?
+        skipped_count += 1
+
+        next
+      end
+
+      unless Invoice.find_by(invoice_number: data_collection[:invoice_number]).nil?
+        skipped_count += 1
+
+        next
+      end
+
+      log_request(invoice_number: data_collection[:invoice_number],
+                  initiator: data_collection[:initiator],
+                  transaction_amount: data_collection[:transaction_amount])
+
+      generate_invoice(attrs: data_collection)
+
+      added_count += 1
+    end
+
+    [added_count, skipped_count]
+  end
+
+  def collect_response_data(data)
+    status = data['status']
+    status = 'unpaid' if status == 'issued'
+
+    {
+      invoice_number: data['invoice_number'],
+      initiator: data['initiator'],
+      transaction_amount: data['transaction_amount'],
+      in_directo: data['in_directo'],
+      e_invoice_sent_at: data['e_invoice_sent_at'],
+      transaction_time: data['transaction_time'],
+      status: status
+    }
+  end
+
+  def generate_invoice(attrs:)
+    invoice = Invoice.new
+    invoice.invoice_number = attrs[:invoice_number]
+    invoice.initiator = attrs[:initiator]
+    invoice.transaction_amount = attrs[:transaction_amount]
+    invoice.status = attrs[:status]
+    invoice.in_directo = attrs[:in_directo]
+    invoice.sent_at_omniva = attrs[:e_invoice_sent_at]
+    invoice.transaction_time = attrs[:transaction_time]
+
+    invoice.save!
+  end
+
+  def log_request(invoice_number:, initiator:, transaction_amount:)
+    Rails.logger.info invoice_number
+    Rails.logger.info initiator
+    Rails.logger.info transaction_amount
+    Rails.logger.info "++++++++++++++++++"
+  end
+end
