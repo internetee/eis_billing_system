94475da6a431f639d9a2fa740de5d0f2f7271401
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2022-09-12 16:35:42 +0300

    fixed restrictions for callback handler

--- app/controllers/api/v1/callback_handler/callback_handler_controller.rb
+++ app/controllers/api/v1/callback_handler/callback_handler_controller.rb
@@ -2,6 +2,8 @@ module Api
   module V1
     module CallbackHandler
       class CallbackHandlerController < ApplicationController
+        skip_before_action :authorized
+
         def callback
           payment_reference = params[:payment_reference]
           response = EverypayResponse.send_request(payment_reference)
