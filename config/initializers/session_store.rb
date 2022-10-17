27fa44273afae49d5d04d662c9186362374d9d5d
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2022-10-17 15:53:08 +0300

    updated ui, added sessions expire

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+EisBillingSystem::Application.config.session_store :cookie_store, 
+                                          :key => '_my_session', 
+                                          :expire_after => 30.minutes
