0f281e141d6d6e0b5455dcc7c16e9822160db750
Oleg Hasjanov <oleg.phenomenon@gmail.com>
2023-07-20 13:56:01 +0300

    refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,8 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :email,
-            presence: true,
-            uniqueness: true,
-            format: { with: /\A[^@\s]+@[^@\s]+\z/,
-                      message: 'Invalid email' }
+  validates :email, presence: true, uniqueness: true,
+            format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
 
   pg_search_scope :search_by_email,
                   against: [:email],
